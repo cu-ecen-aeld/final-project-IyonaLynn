a998e72e7b67770a1780d269468061485ad0e068
iyno9846 <iyno9846@Ubuntu.myguest.virtualbox.org>
2025-02-22 15:50:14 -0700

    gitignore update

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '297be1047e66385c24638e6435f2e3df2771e4fa'
+AESD_ASSIGNMENTS_VERSION = '260c383050ab587ee58224cb9e4d9ccf55136645'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
