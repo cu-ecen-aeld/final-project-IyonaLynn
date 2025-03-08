d446c55c4610bc347f966e2c01be793d32fc019c
iyno9846 <iyno9846@Ubuntu.myguest.virtualbox.org>
2025-03-08 01:34:57 -0700

    Initial Edit A7P2

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,26 @@
+
+##############################################################
+#
+# LDD
+#
+##############################################################
+
+LDD_VERSION = 'edfa61fa7a87bc9f3006810556b6afd6746eb055'
+# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+# with ssh keys and the automated build/test system.
+# Your site should start with git@github.com:
+LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-IyonaLynn.git'
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+
+LDD_MODULE_SUBDIRS = misc-modules scull
+
+define LDD_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 $(@D)/scull/scull_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/scull/scull_unload $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_load $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/misc-modules/module_unload $(TARGET_DIR)/usr/bin
+endef
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
