b25a560e786bc76d3335fa56317e94e9ed3fadb9
iyno9846 <iyno9846@Ubuntu.myguest.virtualbox.org>
2025-03-08 02:16:26 -0700

    runqemu

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '7effa760e948b42a554073233738bfa4ab0bd0ba'
+AESD_ASSIGNMENTS_VERSION = '31e33e6909676e2c51121859d580726b60b4be03'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
