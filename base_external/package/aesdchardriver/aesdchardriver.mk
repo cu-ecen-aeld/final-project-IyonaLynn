6f8ac3630b3a747b9cc80b06f2bfa432ace1d4ff
iyno9846 <iyno9846@Ubuntu.myguest.virtualbox.org>
2025-03-26 17:44:07 -0600

    Update Versions

--- base_external/package/aesdchardriver/aesdchardriver.mk
+++ base_external/package/aesdchardriver/aesdchardriver.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESDCHARDRIVER_VERSION = '6ee10e88c9165e14ca5e358233ce35a3201c467e'
+AESDCHARDRIVER_VERSION = '254f6763d613592c97ea923b4d1a2d686fd8dd42'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
