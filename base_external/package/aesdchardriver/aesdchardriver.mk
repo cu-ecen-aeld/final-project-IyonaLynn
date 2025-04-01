62d5b6724beac6a99169a911ced58e15971e7454
iyno9846 <iyno9846@Ubuntu.myguest.virtualbox.org>
2025-03-31 22:46:18 -0600

    Test Socket

--- base_external/package/aesdchardriver/aesdchardriver.mk
+++ base_external/package/aesdchardriver/aesdchardriver.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-AESDCHARDRIVER_VERSION = '49eab8a30ca9ea15a55e9268e96cbbf7ec0974c3'
+AESDCHARDRIVER_VERSION = '3570983eb2e539ef5f737085b250145a718f71c3'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
