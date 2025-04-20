ea1702198ff0dc94fcdb3fb56a82350bb182497e
iyno9846 <iyno9846@Ubuntu.myguest.virtualbox.org>
2025-04-20 14:48:25 -0600

    Update2

--- base_external/package/aesd-project/aesd-project.mk
+++ base_external/package/aesd-project/aesd-project.mk
@@ -4,19 +4,19 @@
 #
 ##############################################################
 
-AESD_PROJECT_VERSION = 'cb8c4b963facc740097a6873061564d5dbd0a062'
+AESD_PROJECT_VERSION = 'bc13092158b41d52bbb631a103d71cecc5c9b77a'
 AESD_PROJECT_SITE = git@github.com:IyonaLynn/Accelerometer_Character_Device_Driver.git
 AESD_PROJECT_SITE_METHOD = git
 AESD_PROJECT_GIT_SUBMODULES = YES
 
 define AESD_PROJECT_BUILD_CMDS
-#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/driver all
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/accl all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/gpio all
 endef
 
 define AESD_PROJECT_INSTALL_TARGET_CMDS
-	# Install user-space app
 	$(INSTALL) -m 0755 $(@D)/accl/i2c_read_app $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/gpio/gpio_app_gpiod $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
