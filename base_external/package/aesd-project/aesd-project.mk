2f0b939ea1ae48d5bfe8841ee4f790eaaa8e30b5
iyno9846 <iyno9846@Ubuntu.myguest.virtualbox.org>
2025-04-27 15:40:56 -0600

    Make file changes

--- base_external/package/aesd-project/aesd-project.mk
+++ base_external/package/aesd-project/aesd-project.mk
@@ -4,21 +4,30 @@
 #
 ##############################################################
 
-AESD_PROJECT_VERSION = 'cefff7e72ab296d6a202bbfd45bc56952f6334e5'
-AESD_PROJECT_SITE = git@github.com:IyonaLynn/Accelerometer_Character_Device_Driver.git
-AESD_PROJECT_SITE_METHOD = git
-AESD_PROJECT_GIT_SUBMODULES = YES
+AESD_PROJECT_VERSION         = '8f533aa1f3a9bcb1912eeef39dc6eb07407efd9a'
+AESD_PROJECT_SITE            = git@github.com:IyonaLynn/Accelerometer_Character_Device_Driver.git
+AESD_PROJECT_SITE_METHOD     = git
+AESD_PROJECT_GIT_SUBMODULES  = YES
+
+# GUI related
+AESD_PROJECT_ACCEL_GUI_CFLAGS = -Wall -Wextra -g \
+    $(shell $(PKG_CONFIG_HOST_BINARY) --cflags gtk+-3.0)
+AESD_PROJECT_ACCEL_GUI_LDFLAGS = \
+    $(shell $(PKG_CONFIG_HOST_BINARY) --libs gtk+-3.0) -lgpiod -lm
 
 define AESD_PROJECT_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/lsm9ds1_test all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/gpio_test all
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/accel_gui all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/gpio_test   all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) \
+	    CFLAGS="$(TARGET_CFLAGS) $(AESD_PROJECT_ACCEL_GUI_CFLAGS)" \
+	    LDFLAGS="$(TARGET_LDFLAGS) $(AESD_PROJECT_ACCEL_GUI_LDFLAGS)" \
+	    -C $(@D)/accel_gui all
 endef
 
 define AESD_PROJECT_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0755 $(@D)/lsm9ds1_test/lsm9ds1_test $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/gpio_test/gpio_test $(TARGET_DIR)/usr/bin
-	$(INSTALL) -m 0755 $(@D)/accel_gui/accel_gui $(TARGET_DIR)/usr/bin	
+	$(INSTALL) -m 0755 $(@D)/lsm9ds1_test/lsm9ds1_test  $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/gpio_test/gpio_test        $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/accel_gui/accel_gui        $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
