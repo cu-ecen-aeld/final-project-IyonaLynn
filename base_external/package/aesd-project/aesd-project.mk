114389d7c2b92ea80b3455a6e35b1f0b6348a4dd
iyno9846 <iyno9846@Ubuntu.myguest.virtualbox.org>
2025-04-28 19:02:16 -0600

    Updated hash

--- base_external/package/aesd-project/aesd-project.mk
+++ base_external/package/aesd-project/aesd-project.mk
@@ -4,7 +4,7 @@
 #
 ##############################################################
 
-AESD_PROJECT_VERSION         = 'dcb17ba001a479dc5bd747c108e24a27f624e61d'
+AESD_PROJECT_VERSION         = '9165b808c62fcd4caa51e4d555dd050127af1ef2'
 AESD_PROJECT_SITE            = git@github.com:IyonaLynn/Accelerometer_Character_Device_Driver.git
 AESD_PROJECT_SITE_METHOD     = git
 AESD_PROJECT_GIT_SUBMODULES  = YES
